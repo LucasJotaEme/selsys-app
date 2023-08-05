d46140cf0e25769a977ce74a478a36babda8681f
LucasJotaEme <lucasmaldonado10@hotmail.com>
2023-08-05 17:44:13 -0300

    Change logic of files

--- src/Handler/UploadHandler.php
+++ src/Handler/UploadHandler.php
@@ -4,6 +4,7 @@ namespace App\Handler;
 
 use App\Common\GlobalHandler;
 use App\Entity\Upload;
+use Exception;
 
 class UploadHandler extends GlobalHandler
 {
@@ -14,12 +15,14 @@ class UploadHandler extends GlobalHandler
     const ID_PARAM    = "uploadId";
 
     const PROFILE_PHOTO_FILE_TYPE  = "profilePhoto";
+    const DEFAULT = "default";
 
     public function set($params, $type, $edit = false)
     {
         $id    = $params[self::ID_PARAM] ?? null;
         $file  = $params[self::FILE_PARAM] ?? null;
-        $route = $this->getParameter("upload_route");
+        $route = $this->getRoute();
+        $hash  = bin2hex(openssl_random_pseudo_bytes(16));
 
         if (null !== $id) {
             $upload = $this->ifExistsGetById($id, self::ENTITY_NAME);
@@ -27,16 +30,53 @@ class UploadHandler extends GlobalHandler
         } else {
             $upload = new Upload();
             $upload->setCreatedAt(new \DateTimeImmutable());
-            $hash  = bin2hex(openssl_random_pseudo_bytes(16));
         }
-        $file->move($route . "$type/$hash\/", $file->getClientOriginalName());
+
         $upload->setType($type);
-        $upload->setHash($hash);
-        $upload->setFileName($file->getClientOriginalName());
+        if ($file === self::DEFAULT) {
+            $this->createDefaultFile($route,$type,$hash,$upload);
+        } else {
+            $this->cleanFolder($route . "$type/$hash");
+            $file->move($route . "$type/$hash\/", $file->getClientOriginalName());
+            $upload->setHash($hash);
+            $upload->setFileName($file->getClientOriginalName());
+        }
 
         return $this->beforeSave($upload);
     }
 
+    private function createDefaultFile($route, $type, $hash, &$upload)
+    {
+        $sourceFile = $route . "$type/default/" . self::DEFAULT . ".jpg";
+        $destinationDirectory = $route . "$type/$hash/";
+        $destinationFile = $destinationDirectory . self::DEFAULT . ".jpg";
+
+        if (!file_exists($destinationDirectory)) {
+            if (mkdir($destinationDirectory, 0777, true)) {
+                if (copy($sourceFile, $destinationFile)) {
+                    $upload->setHash($hash);
+                    $upload->setFileName(self::DEFAULT . ".jpg");
+                }
+            }
+        } else {
+            if (copy($sourceFile, $destinationFile)) {
+                $upload->setHash($hash);
+                $upload->setFileName(self::DEFAULT . ".jpg");
+            } else {
+                throw new Exception("Error copying default image");
+            }
+        }
+    }
+
+
+    private function cleanFolder($route)
+    {
+        $files = glob("$route/*");
+        foreach ($files as $file) {
+            if (is_file($file))
+                unlink($file);
+        }
+    }
 
     public function beforeSave(Upload $upload)
     {
@@ -45,4 +85,8 @@ class UploadHandler extends GlobalHandler
         return $upload;
     }
 
+    public function getRoute()
+    {
+        return $this->getParameter("upload_route");
+    }
 }
--- src/Handler/UserHandler.php
+++ src/Handler/UserHandler.php
@@ -7,7 +7,8 @@ use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class UserHandler extends GlobalHandler{
+class UserHandler extends GlobalHandler
+{
 
     const ENTITY_NAME = "User";
     const ID_PARAM = "userId";
@@ -18,34 +19,35 @@ class UserHandler extends GlobalHandler{
         parent::__construct($entityManager);
     }
 
-    public function set($params, UserPasswordHasherInterface $passwordHasher = null, $edit = false){
+    public function set($params, UserPasswordHasherInterface $passwordHasher = null, $edit = false)
+    {
         $id = $params[self::ID_PARAM] ?? 0;
         $psw = $params[self::PASSWORD_PARAM] ?? null;
         $file = $params[UploadHandler::FILE_PARAM] ?? null;
         $user = $edit ? $this->ifExistsGetById($id, self::ENTITY_NAME) : new User();
 
-        if($edit){
-            $this->updateUserUpload($user, $file);
-        }
-
-        if(isset($params["email"])){
+        if (isset($params["email"])) {
             $user->setEmail($params["email"]);
         }
-        
-        if($this->validatePassword($psw) && null !== $passwordHasher){
-            $user->setPassword($passwordHasher->hashPassword($user,$psw));   
+
+        if ($this->validatePassword($psw) && null !== $passwordHasher) {
+            $user->setPassword($passwordHasher->hashPassword($user, $psw));
         }
+
+        $this->updateUserUpload($user, $file, !$edit);
 
         return $user;
     }
 
-    public function beforeSave(User $user){
+    public function beforeSave(User $user)
+    {
         $this->validate($user);
         $this->repository(self::ENTITY_NAME)->save($user, true);
         return $user;
     }
 
-    public function remove($params){
+    public function remove($params)
+    {
         $id = $params[self::ID_PARAM] ?? 0;
 
         $this->repository(self::ENTITY_NAME)->remove($this->ifExistsGetById($id, self::ENTITY_NAME), true);
@@ -53,40 +55,42 @@ class UserHandler extends GlobalHandler{
         return $this->getMessageDeleted($id, self::ENTITY_NAME);
     }
 
-    public function get($params){
+    public function get($params)
+    {
         $id = $params[self::ID_PARAM] ?? 0;
 
         return $this->ifExistsGetById($id, self::ENTITY_NAME);
     }
 
-    private function validate(User $user){
+    private function validate(User $user)
+    {
         $repository = $this->repository(self::ENTITY_NAME);
         $userAlreadyExists = $repository->findOneBy(array("email" => $user->getEmail()));
 
-        if(null !== $userAlreadyExists && $userAlreadyExists->getId() != $user->getId()){
+        if (null !== $userAlreadyExists && $userAlreadyExists->getId() != $user->getId()) {
             throw new \Exception($this->getMessageAlreadyExists($user->getEmail(), self::ENTITY_NAME));
         }
     }
 
-    private function validatePassword($psw):bool{
+    private function validatePassword($psw): bool
+    {
         // Se lo deja en una función ya que en un futuro pueden haber más validaciones que hacer en password.
         // Chequear si es necesario trasladar a GlobalHandler.
         return null !== $psw;
     }
 
-    private function updateUserUpload(User $user, $file)
-{
-    $uploadId = null !== $user->getUpload() ? $user->getUpload()->getId() : null;
+    private function updateUserUpload(User $user, $file, $byDefault = false)
+    {
+        $uploadId = null !== $user->getUpload() ? $user->getUpload()->getId() : null;
+        $type = UploadHandler::PROFILE_PHOTO_FILE_TYPE;
 
-    if ($file) {
         $upload = $this->uploadHandler->set(
             [
-                "file" => $file,
-                UploadHandler::ID_PARAM => $uploadId
+                "file" => $byDefault ? UploadHandler::DEFAULT : $file,
+                UploadHandler::ID_PARAM => $uploadId,
             ],
-            UploadHandler::PROFILE_PHOTO_FILE_TYPE
+            $type
         );
         $user->setUpload($upload);
     }
-}
 }
